e82bacda6fd2a3d80b195779b02d42ef2cdb15b7
Ahmar Suhail <ahmarsu@amazon.co.uk>
2022-11-22 15:37:15 +0000

    updates S3Object import

--- hadoop-tools/hadoop-aws/src/main/java/org/apache/hadoop/fs/s3a/S3AFileSystem.java
+++ hadoop-tools/hadoop-aws/src/main/java/org/apache/hadoop/fs/s3a/S3AFileSystem.java
@@ -88,6 +88,7 @@ import software.amazon.awssdk.services.s3.model.ObjectIdentifier;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import software.amazon.awssdk.services.s3.model.PutObjectResponse;
 import software.amazon.awssdk.services.s3.model.S3Error;
+import software.amazon.awssdk.services.s3.model.S3Object;
 import software.amazon.awssdk.services.s3.model.SelectObjectContentRequest;
 import software.amazon.awssdk.services.s3.model.SelectObjectContentResponseHandler;
 import software.amazon.awssdk.services.s3.model.StorageClass;
@@ -2741,9 +2742,7 @@ public class S3AFileSystem extends FileSystem implements StreamCapabilities,
               OBJECT_CONTINUE_LIST_REQUEST,
               () -> {
                 if (useListV1) {
-                  //TODO: Update to List<S3Object> once we can get rid of the other S3Object import
-                  List<software.amazon.awssdk.services.s3.model.S3Object>
-                      prevListResult = prevResult.getV1().contents();
+                  List<S3Object> prevListResult = prevResult.getV1().contents();
 
                   // Next markers are only present when a delimiter is specified.
                   String nextMarker;
